de307c93e4c14efd8c31b51916a4e2952731c565
sar205 <156918300+sar205@users.noreply.github.com>
2024-08-20 00:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const allowedOrigins = ['http://localhost:5173', '*'];
+const allowedOrigins = ['*'];
 
 app.use(cors({
     origin: function (origin, callback) {
